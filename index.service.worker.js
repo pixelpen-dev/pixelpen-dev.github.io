ccdb67e39663696458d5e6a3463f6c48436ab27f
bayu-sw <bayusantoso08@gmail.com>
2024-06-17 21:49:45 +0700

    v0.2.3

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1718407300|112109306";
+const CACHE_VERSION = "1718633667|272679648";
 const CACHE_PREFIX = "PixelPen-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
