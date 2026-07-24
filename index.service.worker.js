20cb51c1c0827c8d946c54946aef8364689eb004
Bayu Santoso Widodo <bayusantoso08@gmail.com>
2026-07-24 13:14:48 +0700

    Sync

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784450076|397978947';
+const CACHE_VERSION = '1784873339|29306792';
 /** @type {string} */
 const CACHE_PREFIX = 'PixelPen-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
